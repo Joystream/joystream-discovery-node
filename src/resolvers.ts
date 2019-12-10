d8247d857a0d189c3929e63210ed2182cdd91fc3
Kulpreet Singh <ksinghf@jibber>
2019-12-10 12:34:23 +0100

    Fix category resolver to return the correct array

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -18,8 +18,7 @@
 export default {
     Query: {
         getCategories: async (root: any, args: any, context: any) => {
-            const categories = await context.dataSources.categories.getCategories();
-            return [categories]
+            return await context.dataSources.categories.getCategories();
         }
     }
 };
